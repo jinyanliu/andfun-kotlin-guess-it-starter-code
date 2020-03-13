1112d1c979cbef04cc2013ee5c7b7ef1e43eed08
Jinyan Liu <jinyan.liu@truecaller.com>
2020-03-14 00:23:24 +0100

    Add Buzz

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -16,11 +16,14 @@
 
 package com.example.android.guesstheword.screens.game
 
+import android.os.Build
 import android.os.Bundle
-import android.text.format.DateUtils
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -64,10 +67,26 @@ class GameFragment : Fragment() {
             }
         })
 
-        viewModel.currentTime.observe(viewLifecycleOwner, Observer { currentTime ->
-            binding.timerText.text = DateUtils.formatElapsedTime(currentTime)
+        viewModel.buzzType.observe(viewLifecycleOwner, Observer { type ->
+            if (type != GameViewModel.BuzzType.NO_BUZZ) {
+                buzz(type.pattern)
+                viewModel.onBuzzFinish()
+            }
         })
 
         return binding.root
+    }
+
+    private fun buzz(pattern: LongArray) {
+        val buzzer = activity?.getSystemService<Vibrator>()
+
+        buzzer?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                buzzer.vibrate(VibrationEffect.createWaveform(pattern, -1))
+            } else {
+                //deprecated in API 26
+                buzzer.vibrate(pattern, -1)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -8,11 +8,17 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
+private val CORRECT_BUZZ_PATTERN = longArrayOf(100, 100, 100, 100, 100, 100)
+private val PANIC_BUZZ_PATTERN = longArrayOf(0, 200)
+private val GAME_OVER_BUZZ_PATTERN = longArrayOf(0, 2000)
+private val NO_BUZZ_PATTERN = longArrayOf(0)
+
 class GameViewModel: ViewModel() {
 
     companion object {
         private const val DONE = 0L
         private const val ONE_SECOND = 1000L
+        private const val PANIC_SECOND = 3000L
         private const val COUNTDOWN_TIME = 10000L
     }
 
@@ -38,6 +44,10 @@ class GameViewModel: ViewModel() {
         DateUtils.formatElapsedTime(time)
     }
 
+    private val _buzzType = MutableLiveData<BuzzType>()
+    val buzzType: LiveData<BuzzType>
+        get() = _buzzType
+
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
@@ -50,11 +60,15 @@ class GameViewModel: ViewModel() {
         timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
             override fun onTick(millisUntilFinished: Long) {
                 _currentTime.value = millisUntilFinished / ONE_SECOND
+                if (millisUntilFinished <= PANIC_SECOND) {
+                    _buzzType.value = BuzzType.COUNTDOWN_PANIC
+                }
             }
 
             override fun onFinish() {
                 _currentTime.value = DONE
                 _eventGameFinish.value = true
+                _buzzType.value = BuzzType.GAME_OVER
             }
         }
         timer.start()
@@ -106,11 +120,23 @@ class GameViewModel: ViewModel() {
     }
 
     fun onCorrect() {
+        _buzzType.value = BuzzType.CORRECT
         _score.value = (score.value)?.plus(1)
         nextWord()
     }
 
     fun onGameFinishComplete() {
         _eventGameFinish.value = false
+    }
+
+    fun onBuzzFinish() {
+        _buzzType.value = BuzzType.NO_BUZZ
+    }
+
+    enum class BuzzType(val pattern: LongArray) {
+        CORRECT(CORRECT_BUZZ_PATTERN),
+        GAME_OVER(GAME_OVER_BUZZ_PATTERN),
+        COUNTDOWN_PANIC(PANIC_BUZZ_PATTERN),
+        NO_BUZZ(NO_BUZZ_PATTERN)
     }
 }
